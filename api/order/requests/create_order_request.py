2220151ad70bed89f3a8e9bc3c3836a3d6ab1ecc
Saad Adel <saad.adel539@yahoo.com>
2023-12-26 18:30:12 +0200

    cont.

--- api/order/requests/create_order_request.py
+++ api/order/requests/create_order_request.py
@@ -3,22 +3,27 @@ from models.base.enums import DeliverStatus, Language
 
 
 class CreateOrderRequest(BaseModel):
-    oodi_mobile: str = Field(default=None, examples=["9002213"])
-    user_shortname: str = Field(default=None, examples=["1fd1cc82"])
-    plan_shortname: str = Field(default=None, examples=["ecc82efb"])
+    oodi_mobile: str | None = None
+    user_shortname: str | None = None
+    plan_shortname: str | None = None
     addons: list[str] = []
     high5: bool
     language: Language = Field(
         default=Language.en, examples=[Language.en, Language.ar, Language.kd]
     )
-    state: DeliverStatus = Field(
-        default=DeliverStatus.pending,
-        examples=[
-            DeliverStatus.pending,
-            DeliverStatus.assigned,
-            DeliverStatus.cancelled,
-            DeliverStatus.confirmed,
-            DeliverStatus.failed,
-            DeliverStatus.rejected,
-        ],
-    )
+    state: DeliverStatus = DeliverStatus.pending
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "oodi_mobile": "9002213",
+                    "user_shortname": "1fd1cc82",
+                    "plan_shortname": "ecc82efb",
+                    "addons": ["addon_one", "addon_two"],
+                    "high5": True,
+                    "language": "en",
+                }
+            ]
+        }
+    }
--- api/order/requests/update_order.py
+++ api/order/requests/update_order.py
@@ -1,38 +1,45 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from models.base.enums import DeliverStatus, DeliveryMethod
-from datetime import datetime
 
 from models.order import Delivery
 
 
 class UpdateOrderRequest(BaseModel):
-    tracking_id: str = Field(examples=["3440555"])
-    planned_delivery_date: str = Field(default=None, examples=[datetime.now()])
-    scheduled_delivery: str = Field(default=None, examples=[datetime.now()])
-    delivery: Delivery = Field(
-        examples=[
-            {"method": DeliveryMethod.store_pickup, "store_shortname": "8ba3ef81"},
-            {
-                "method": DeliveryMethod.store_pickup,
-                "address": {
-                    "city": "Cairo",
-                    "governorate_shortname": "Heliopolis",
-                    "district_shortname": "D3",
-                    "street": "Ali Basha",
-                    "building": "Tarra",
-                    "apartment": "33",
-                    "location": {"latitude": 33.3152, "longitude": 44.3661},
+    tracking_id: str
+    planned_delivery_date: str | None = None
+    scheduled_delivery: str | None = None
+    delivery: Delivery
+    state: DeliverStatus = DeliverStatus.pending
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "tracking_id": "3440555",
+                    "planned_delivery_date": "2023-12-26T18:22:13.532433",
+                    "scheduled_delivery": "2023-12-26T18:22:13.532461",
+                    "delivery": {
+                        "method": DeliveryMethod.home,
+                        "address": {
+                            "city": "Cairo",
+                            "governorate_shortname": "Heliopolis",
+                            "district_shortname": "D3",
+                            "street": "Ali Basha",
+                            "building": "Tarra",
+                            "apartment": "33",
+                            "location": {"latitude": 33.3152, "longitude": 44.3661},
+                        },
+                    },
                 },
-            },
-        ]
-    )
-    state: DeliverStatus = Field(
-        default=DeliverStatus.pending,
-        examples=[
-            DeliverStatus.assigned,
-            DeliverStatus.cancelled,
-            DeliverStatus.confirmed,
-            DeliverStatus.failed,
-            DeliverStatus.rejected,
-        ],
-    )
+                {
+                    "tracking_id": "3440555",
+                    "planned_delivery_date": "2023-12-26T18:22:13.532433",
+                    "scheduled_delivery": "2023-12-26T18:22:13.532461",
+                    "delivery": {
+                        "method": DeliveryMethod.store_pickup,
+                        "store_shortname": "8ba3ef81",
+                    },
+                },
+            ]
+        }
+    }
